5f5aaa8910d1f8da726b4b9a6207a0714b780d83
billdeptrai0512 <billnguyen051298@gmail.com>
2024-01-31 15:37:16 +0700

    Add required validation for user to input book

--- java.js
+++ java.js
@@ -7,7 +7,7 @@ class Book {
     this.status = status;
   }
 
-  showBook() {
+  get showBook() {
     const bookCard = document.createElement('div');
     bookCard.className = 'book-card';
 
@@ -75,90 +75,36 @@ const bookGrid = document.querySelector('.book-grid');
 const checkBox = diaLog.querySelector('#read');
 
 
+
 showButton.addEventListener('click', () => {
   diaLog.showModal();
 })
 
+
+
+
+console.log(title)
+
+function checkValidation(title, author, page, read) {
+  if (title.checkValidity() && author.checkValidity() && page.checkValidity()) {
+    return true
+  }
+  return false
+}
+
 confirmButton.addEventListener('click', (e) => {
+  if (checkValidation(title, author, page)) {
+    let book = new Book(title.value, author.value, page.value, read.checked);
+    myLibrary.push(book);
+    book.showBook;
+    form.reset();
+    diaLog.close();
+    return
+  }
   e.preventDefault();
-  let book = new Book(title.value, author.value, page.value, read.checked);
-  myLibrary.push(book);
-  book.showBook();
-  form.reset();
-  diaLog.close();
 })
 
 
-// function Book(title, author, page, status) {
-//   this.title = title;
-//   this.author = author;
-//   this.page = page;
-//   this.status = status;
-//   // the constructor...
-// }
-
-// function addBookToLibrary() {
-//   bookGrid.innerHTML = '';
-//   for (let i = 0; i < myLibrary.length; i++) {
-//     showBook(myLibrary[i]);
-//   }
-// }
-
-// function showBook(book) {
-//   const bookCard = document.createElement('div');
-//   bookCard.className = 'book-card';
-  
-//   const bookTitle = document.createElement('p');
-//   bookTitle.className = 'book-title';
-//   bookTitle.textContent = book.title;
-//   bookCard.appendChild(bookTitle);
-
-//   const bookAuthor = document.createElement('p');
-//   bookAuthor.className = 'book-author';
-//   bookAuthor.textContent = book.author;
-//   bookCard.appendChild(bookAuthor);
-
-//   const bookPage = document.createElement('p');
-//   bookPage.className = 'book-title';
-//   bookPage.textContent = book.page;
-//   bookCard.appendChild(bookPage);
-
-//   const bookRead = document.createElement('button');
-//   bookRead.className = 'book-read';
-//   if (book.status === true) {
-//     bookRead.classList.add('done')
-//     bookRead.textContent = "Read"
-//   } else {
-//     bookRead.classList.remove('done')
-//     bookRead.textContent = "Not Read"
-//   } 
-
-
-//   bookCard.appendChild(bookRead)
-
-//   bookRead.addEventListener('click', () => {
-//     book.status = (!book.status);
-//     if (book.status == true) {
-//       bookRead.classList.add('done');
-//       bookRead.textContent = "Read";
-//     } else {
-//       bookRead.classList.remove('done');
-//       bookRead.textContent = "Not Read"
-//     }
-//   })
-
-//   const bookRemove = document.createElement('button');
-//   bookRemove.className = 'book-remove';
-//   bookRemove.textContent = "Remove";
-//   bookCard.appendChild(bookRemove);
-
-//   bookRemove.addEventListener('click', () => {
-//     bookCard.remove();
-//     myLibrary.splice(myLibrary.indexOf('${book.title}'), 1);
-//   });
-
-//   bookGrid.appendChild(bookCard);
-// }
 
 
 
